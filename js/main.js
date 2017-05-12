4dc7f34b9d39940b476b1f50477195ec3cf9dac9
Natalia Garrido <natgramirez@outlook.com>
2017-05-11 23:46:51 -0300

    estructurando el formulario y font

---
+++ js/main.js
@@ -0,0 +1,4 @@
+// Debe contener las validaciones del formulario y desarrollar la función que muestra los datos en la seccion siguiente.
+// - Ningun campo puede quedar vacio
+// - El checkbox debe estar seleccionado para enviar los datos y el select debe tener seleccionada una opcion.
+// - Completar la funcion que muestre los datos del usuario ingresados en el formulario.
